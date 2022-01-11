c90e2507b68d5f61df7646f136b21857040c4d66
Damita Salmon <lainofthesith@gmail.com>
2022-01-10 21:13:28 -0500

    Organize test files

--- checkArqFailure.sh
+++ checkArqFailure.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-OUTPUT_LOG="/Users/Shared/arqLog2"
+OUTPUT_LOG="/Users/Shared/arqLog"
 
 /bin/cat /Library/Application\ Support/ArqAgent/logs/backup/$(ls -1tr /Library/Application\ Support/ArqAgent/logs/backup/ | tail -n 1) > $OUTPUT_LOG
-
+#if you find these words, all good. Else Backup failure.
 if grep -q 'Uploaded|No changes' $OUTPUT_LOG; then
-  echo true
+  echo false
 else
-    echo false
+    echo true
 fi
 
 exit 0
